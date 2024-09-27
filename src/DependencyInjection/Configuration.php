525d36964db22f01d58a0a7f508b623083f15fad
Silas Joisten <silasjoisten@proton.me>
2024-09-27 09:46:33 +0300

    Fix

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,8 +2,18 @@
 
 declare(strict_types=1);
 
+/**
+ * This file is part of sensiolabs-de/storyblok-api-bundle.
+ *
+ * (c) SensioLabs Deutschland <info@sensiolabs.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace SensioLabs\Storyblok\Bundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
