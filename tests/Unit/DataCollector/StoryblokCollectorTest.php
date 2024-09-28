ba6d5e3f3f67df2bd8a549fc218940a6ce76fe10
Silas Joisten <silasjoisten@proton.me>
2024-09-28 14:35:48 +0300

    Fix

---
+++ tests/Unit/DataCollector/StoryblokCollectorTest.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of sensiolabs-de/storyblok-bundle.
+ *
+ * (c) SensioLabs Deutschland <info@sensiolabs.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SensioLabs\Storyblok\Bundle\Tests\Unit\DataCollector;
+
+use PHPUnit\Framework\TestCase;
+use SensioLabs\Storyblok\Bundle\DataCollector\StoryblokCollector;
+use SensioLabs\Storyblok\Bundle\Tests\Util\FakerTrait;
+use Symfony\Component\HttpClient\MockHttpClient;
+use Symfony\Component\HttpClient\Response\JsonMockResponse;
+use Symfony\Component\HttpClient\TraceableHttpClient;
+
+final class StoryblokCollectorTest extends TestCase
+{
+    use FakerTrait;
+
+    /**
+     * @test
+     */
+    public function defaults(): void
+    {
+        $client = new TraceableHttpClient(new MockHttpClient());
+        $collector = new StoryblokCollector($client);
+
+        self::assertSame([], $collector->getTraces());
+        self::assertSame(0, $collector->getRequestCount());
+        self::assertSame(0, $collector->getErrorCount());
+    }
+
+    /**
+     * @test
+     */
+    public function getTemplate(): void
+    {
+        self::assertSame('@Storyblok/data_collector.html.twig', StoryblokCollector::getTemplate());
+    }
+
+    /**
+     * @test
+     */
+    public function lateCollect(): void
+    {
+        $client = new TraceableHttpClient(new MockHttpClient([
+            new JsonMockResponse(['hello' => 'there'], ['http_code' => 200]),
+        ]));
+
+        $client->request('GET', 'https://example.com');
+
+        $collector = new StoryblokCollector($client);
+
+        $collector->lateCollect();
+
+        self::assertCount(1, $collector->getTraces());
+        self::assertSame(1, $collector->getRequestCount());
+        self::assertSame(0, $collector->getErrorCount());
+    }
+}
