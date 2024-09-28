9f80b79467326cd326a2771f9a2da94295fc8bb0
Silas Joisten <silasjoisten@proton.me>
2024-09-28 11:48:47 +0300

    Adds Test

---
+++ tests/Unit/DependencyInjection/StoryblokExtensionTest.php
@@ -0,0 +1,105 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of sensiolabs-de/storyblok-bundle.
+ *
+ * (c) SensioLabs Deutschland <info@sensiolabs.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace SensioLabs\Storyblok\Bundle\Tests\Unit\DependencyInjection;
+
+use PHPUnit\Framework\TestCase;
+use SensioLabs\Storyblok\Bundle\DataCollector\StoryblokCollector;
+use SensioLabs\Storyblok\Bundle\DependencyInjection\StoryblokExtension;
+use SensioLabs\Storyblok\Bundle\Listener\UpdateProfilerListener;
+use SensioLabs\Storyblok\Bundle\Tests\Util\FakerTrait;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpClient\TraceableHttpClient;
+
+final class StoryblokExtensionTest extends TestCase
+{
+    use FakerTrait;
+
+    /**
+     * @test
+     */
+    public function loadWillSetParameters(): void
+    {
+        $faker = self::faker();
+
+        $extension = new StoryblokExtension();
+        $builder = new ContainerBuilder();
+        $builder->setParameter('kernel.debug', $faker->boolean());
+
+        $config = [
+            ['base_uri' => $baseUri = $faker->url()],
+            ['token' => $token = $faker->uuid()],
+        ];
+
+        $extension->load(
+            $config,
+            $builder,
+        );
+
+        self::assertSame($baseUri, $builder->getParameter('storyblok_api.base_uri'));
+        self::assertSame($token, $builder->getParameter('storyblok_api.token'));
+    }
+
+    /**
+     * @test
+     */
+    public function loadWithoutKernelDebugWillRemoveDefinitions(): void
+    {
+        $faker = self::faker();
+
+        $extension = new StoryblokExtension();
+        $builder = new ContainerBuilder();
+        $builder->setParameter('kernel.debug', false);
+
+        $config = [
+            ['base_uri' => $faker->url()],
+            ['token' => $faker->uuid()],
+        ];
+
+        $extension->load(
+            $config,
+            $builder,
+        );
+
+        self::assertFalse($builder->hasDefinition(StoryblokCollector::class));
+        self::assertFalse($builder->hasDefinition(UpdateProfilerListener::class));
+    }
+
+    /**
+     * @test
+     */
+    public function loadWithKernelDebugWillReplaceHttpClientWithTracableHttpClient(): void
+    {
+        $faker = self::faker();
+
+        $extension = new StoryblokExtension();
+        $builder = new ContainerBuilder();
+        $builder->setParameter('kernel.debug', true);
+
+        $config = [
+            ['base_uri' => $faker->url()],
+            ['token' => $faker->uuid()],
+        ];
+
+        $extension->load(
+            $config,
+            $builder,
+        );
+
+        self::assertTrue($builder->hasDefinition('storyblok.http_client'));
+
+        $definition = $builder->getDefinition('storyblok.http_client');
+
+        self::assertSame(TraceableHttpClient::class, $definition->getClass());
+    }
+}
