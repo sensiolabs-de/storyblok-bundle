e1247a0959eb3f9acbcb5f17c968afecec31af05
Silas Joisten <silasjoisten@proton.me>
2024-09-28 13:09:34 +0300

    Fix: Fixes alias

--- tests/Unit/DependencyInjection/StoryblokExtensionTest.php
+++ tests/Unit/DependencyInjection/StoryblokExtensionTest.php
@@ -17,6 +17,7 @@ use PHPUnit\Framework\TestCase;
 use SensioLabs\Storyblok\Api\AssetsApi;
 use SensioLabs\Storyblok\Api\AssetsApiInterface;
 use SensioLabs\Storyblok\Api\StoryblokAssetsClient;
+use SensioLabs\Storyblok\Api\StoryblokClientInterface;
 use SensioLabs\Storyblok\Bundle\DataCollector\StoryblokCollector;
 use SensioLabs\Storyblok\Bundle\DependencyInjection\StoryblokExtension;
 use SensioLabs\Storyblok\Bundle\Listener\UpdateProfilerListener;
@@ -129,6 +130,7 @@ final class StoryblokExtensionTest extends TestCase
 
         self::assertFalse($builder->hasDefinition(StoryblokAssetsClient::class));
         self::assertFalse($builder->hasAlias(AssetsApiInterface::class));
+        self::assertFalse($builder->hasAlias(StoryblokClientInterface::class));
         self::assertFalse($builder->hasDefinition(AssetsApi::class));
         self::assertFalse($builder->hasParameter('storyblok_api.assets_token'));
     }
@@ -157,6 +159,7 @@ final class StoryblokExtensionTest extends TestCase
 
         self::assertTrue($builder->hasDefinition(StoryblokAssetsClient::class));
         self::assertTrue($builder->hasAlias(AssetsApiInterface::class));
+        self::assertTrue($builder->hasAlias(StoryblokClientInterface::class));
         self::assertTrue($builder->hasDefinition(AssetsApi::class));
         self::assertTrue($builder->hasParameter('storyblok_api.assets_token'));
         self::assertSame($token, $builder->getParameter('storyblok_api.assets_token'));
