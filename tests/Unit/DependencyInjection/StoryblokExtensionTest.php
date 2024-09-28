87d883496354e0573c4b78a9f5084d546284fa14
Silas Joisten <silasjoisten@proton.me>
2024-09-28 12:55:10 +0300

    Enhancement: Configure `AssetsApi`

--- tests/Unit/DependencyInjection/StoryblokExtensionTest.php
+++ tests/Unit/DependencyInjection/StoryblokExtensionTest.php
@@ -14,6 +14,9 @@ declare(strict_types=1);
 namespace SensioLabs\Storyblok\Bundle\Tests\Unit\DependencyInjection;
 
 use PHPUnit\Framework\TestCase;
+use SensioLabs\Storyblok\Api\AssetsApi;
+use SensioLabs\Storyblok\Api\AssetsApiInterface;
+use SensioLabs\Storyblok\Api\StoryblokAssetsClient;
 use SensioLabs\Storyblok\Bundle\DataCollector\StoryblokCollector;
 use SensioLabs\Storyblok\Bundle\DependencyInjection\StoryblokExtension;
 use SensioLabs\Storyblok\Bundle\Listener\UpdateProfilerListener;
@@ -101,5 +104,61 @@ final class StoryblokExtensionTest extends TestCase
         $definition = $builder->getDefinition('storyblok.http_client');
 
         self::assertSame(TraceableHttpClient::class, $definition->getClass());
+    }
+
+    /**
+     * @test
+     */
+    public function loadWithoutAssetsToken(): void
+    {
+        $faker = self::faker();
+
+        $extension = new StoryblokExtension();
+        $builder = new ContainerBuilder();
+        $builder->setParameter('kernel.debug', true);
+
+        $config = [
+            ['base_uri' => $faker->url()],
+            ['token' => $faker->uuid()],
+        ];
+
+        $extension->load(
+            $config,
+            $builder,
+        );
+
+        self::assertFalse($builder->hasDefinition(StoryblokAssetsClient::class));
+        self::assertFalse($builder->hasAlias(AssetsApiInterface::class));
+        self::assertFalse($builder->hasDefinition(AssetsApi::class));
+        self::assertFalse($builder->hasParameter('storyblok_api.assets_token'));
+    }
+
+    /**
+     * @test
+     */
+    public function loadWithAssetsToken(): void
+    {
+        $faker = self::faker();
+
+        $extension = new StoryblokExtension();
+        $builder = new ContainerBuilder();
+        $builder->setParameter('kernel.debug', true);
+
+        $config = [
+            ['base_uri' => $faker->url()],
+            ['token' => $faker->uuid()],
+            ['assets_token' => $token = $faker->uuid()],
+        ];
+
+        $extension->load(
+            $config,
+            $builder,
+        );
+
+        self::assertTrue($builder->hasDefinition(StoryblokAssetsClient::class));
+        self::assertTrue($builder->hasAlias(AssetsApiInterface::class));
+        self::assertTrue($builder->hasDefinition(AssetsApi::class));
+        self::assertTrue($builder->hasParameter('storyblok_api.assets_token'));
+        self::assertSame($token, $builder->getParameter('storyblok_api.assets_token'));
     }
 }
